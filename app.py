16ac68beeb230a416b15f554d8dfd49c16de74cb
Stefanus Titan <stefanustitan@gmail.com>
2023-11-11 22:57:48 +0700

    Tidier format with Black Formatter (Python)

--- app.py
+++ app.py
@@ -4,30 +4,30 @@ import fuzzy_module  # Import your fuzzy logic module or define functions here
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Get data from the form
         input_data = {
-            'temperature': float(request.form['inputTemperature']),
-            'headache': float(request.form['inputHeadacheLevel']),
-            'eyepain': float(request.form['inputEyepainLevel']),
-            'musclejointpain': float(request.form['inputMuscleJointPainLevel']),
-            'nausea': float(request.form['inputNauseaLevel']),
-            'vomiting': request.form.get('inputVomiting'),
-            'swollenglands': request.form.get('inputSwollenGland'),
-            'rash': request.form.get('inputRash'),
+            "temperature": float(request.form["inputTemperature"]),
+            "headache": float(request.form["inputHeadacheLevel"]),
+            "eyepain": float(request.form["inputEyepainLevel"]),
+            "musclejointpain": float(request.form["inputMuscleJointPainLevel"]),
+            "nausea": float(request.form["inputNauseaLevel"]),
+            "vomiting": request.form.get("inputVomiting"),
+            "swollenglands": request.form.get("inputSwollenGland"),
+            "rash": request.form.get("inputRash"),
         }
 
         # Call the fuzzy logic function
         result, data = fuzzy_module.perform_fuzzy_logic(**input_data)
 
         # Return the result
-        return render_template('index.html', result=result, data=data)
+        return render_template("index.html", result=result, data=data)
 
     # Render the HTML template on GET request
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
